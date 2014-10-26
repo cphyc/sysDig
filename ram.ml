6953e7bfa874427ba2d63ce5b36f7adbfae26c2f
cphyc <admin@cphyc.me>
2014-10-27 00:16:51 +0100

    Use of a stack instead of a mutable list

--- ram.ml
+++ ram.ml
@@ -1,8 +1,9 @@
 open Netlist_ast
 let addr_size = ref (0)
 let word_size = ref (0)
-let ram = ref (Array.make_matrix 0 0 ( ref (0) ))
-	      
+let ram = ref (Array.make_matrix 0 0 false)
+let ram_stack = Stack.create ()
+		    
 let create p =
   (* find the occurences of ERam and check consistency *)
   let iter eq = match eq with 
@@ -14,5 +15,12 @@ let create p =
   List.iter iter p.p_eqs;
   print_endline
     ("Creating RAM of size "^(string_of_int (1 lsl !addr_size))^"*"^(string_of_int !word_size));
-  ram := Array.make_matrix (1 lsl !addr_size) !word_size (ref 0)
+  ram := Array.make_matrix (1 lsl !addr_size) !word_size false
 
+let push write_addr data =
+  Stack.push (write_addr, data) ram_stack
+				       
+let process_queue () =
+  Stack.iter (fun (write_addr, data) -> !ram.(write_addr * !word_size) <- data) ram_stack;
+  Stack.clear ram_stack
+	    
