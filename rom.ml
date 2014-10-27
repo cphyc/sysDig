fcd6b2f560a91a6904b79bf586f48844816a1615
cphyc <admin@cphyc.me>
2014-10-27 14:49:54 +0100

    Refactoring

--- rom.ml
+++ rom.ml
@@ -2,27 +2,30 @@ let rom = ref (Array.make_matrix 0 0 false)
 			    
 let addr_size = ref (0)
 let word_size = ref (0)
-		    
-let load_to_ram address value =
-  assert false
 
-let find_file filename =
-  try
-    open_in filename
-  with
-    | _ -> raise (Parse_error "No such file '%s'")
+let create () =
+  rom := Array.make_matrix (1 lsl !addr_size) !word_size false
+			   
+let load address value =
+  !rom.address <- value
 
 let read_file filename =
+  let find_file filename =
+    try
+      open_in filename
+    with
+    | _ -> raise (Parse_error "No such file '%s'")
+  in
   let ic = find_file filename in
   let buf = Lexing.from_channel ic in
   try
-    Ram_lexer.token buf
+    Rom_lexer.token buf
   with
-  | Ram_lexer.Lexing_error _ ->
+  | Rom_lexer.Lexing_error _ ->
      raise (Failure "Erreur dans l'analyse lexicale")
 	       
-let create_rom filename =
+let create filename =
   let rom_name = (Filename.chop_suffix filename ".net") ^ ".rom" in
-  let rom = open_in rom_name in
-
-  let lexbuf = Lexing.from_channel 
+  read_file rom_name;
+  dump
+  
