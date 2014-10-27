8a2437f79e1ffa686e5b571b8cb281089c96f979
cphyc <admin@cphyc.me>
2014-10-27 17:22:35 +0100

    Changed the list to a stack to load, removed the ref (moved
    to rom_comm.ml)

--- rom.ml
+++ rom.ml
@@ -1,31 +1,22 @@
-let rom = ref (Array.make_matrix 0 0 false)
-			    
-let addr_size = ref (0)
-let word_size = ref (0)
-
-let create () =
-  rom := Array.make_matrix (1 lsl !addr_size) !word_size false
-			   
-let load address value =
-  !rom.address <- value
-
 let read_file filename =
   let find_file filename =
     try
       open_in filename
     with
-    | _ -> raise (Parse_error "No such file '%s'")
+    | _ -> raise (Failure ("No such file "^filename))
   in
   let ic = find_file filename in
   let buf = Lexing.from_channel ic in
   try
-    Rom_lexer.token buf
+    Rom_lexer.token buf;
   with
+  | Rom_lexer.Eof -> ()
   | Rom_lexer.Lexing_error _ ->
      raise (Failure "Erreur dans l'analyse lexicale")
 	       
 let create filename =
   let rom_name = (Filename.chop_suffix filename ".net") ^ ".rom" in
-  read_file rom_name;
-  dump
-  
+  read_file rom_name ;
+  print_endline ("Created ROM of size "^(string_of_int !Rom_comm.addr_size) ^
+		   "*" ^ (string_of_int !Rom_comm.word_size));
+  Array.iteri (fun i v ->  print_string (Interaction.stringint_of_bool v)) !Rom_comm.rom
