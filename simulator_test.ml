98d10e5715d3fb4d6dba2e6527b826043deae9f2
cphyc <admin@cphyc.me>
2014-10-31 16:14:58 +0100

    Major refactoring to handle -v correctly and output the
    scheduled program into a *_sch.net file

--- simulator_test.ml
+++ simulator_test.ml
@@ -6,37 +6,37 @@ let compile filename =
     let p = Netlist.read_file filename in
     let out_name = (Filename.chop_suffix filename ".net") ^ "_sch.net" in
     let out = open_out out_name in
-    let close_all () =
-      close_out out
-    in
-    let p = try
-        Scheduler.schedule p 
-      with
-      | Scheduler.Combinational_cycle ->
-         Format.eprintf "The netlist has a combinatory cycle.@.";
-         close_all ();
-	 exit 2
-      | Netlist.Parse_error s ->
-	 Format.eprintf "An error occurred: %s@." s;
-	 close_all ();
-	 exit 2
-    in
-    close_all ();
     
-    (
-      if not !print_only then (
-	let p_scheduled = Scheduler.schedule p in
-	(* Create the rom *)
-	Rom.create filename;
-	(* Execute the program *)
-	Simulator.execute p_scheduled !number_steps;
-      )
-      else (
-	Netlist_printer.print_program stdout p;
-      )
-    )
+    (* Schedule the program *)
+    let p = Scheduler.schedule p  in
+    (* Write the output to the _sch.net file*)
+    Netlist_printer.print_program out p;
     
-    
+    if  !print_only then
+      (* Also print it to stdout *)
+      Netlist_printer.print_program stdout p
+    else
+      (* Else run it *)
+      let p_scheduled = Scheduler.schedule p in
+      
+      (* Create the rom *)
+      Rom.create filename;
+
+      (* Execute it *)
+      Simulator.execute p_scheduled !number_steps
+      
+  with
+  | Scheduler.Combinational_cycle ->
+     Format.eprintf "The netlist has a combinatory cycle.@.";
+     exit 2
+  | Netlist.Parse_error s ->
+     Format.eprintf "An error occurred: %s@." s;
+     exit 2
+  | _ ->
+     Format.eprintf "Unknown error";
+     exit 2
+
+
 let main () =
   Arg.parse
     ["-v", Arg.Set print_only, "Only print the result of scheduling";
