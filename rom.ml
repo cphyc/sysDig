000449b77ddd93038bceb10f9222d1517ebebaa1
cphyc <admin@cphyc.me>
2014-10-31 16:13:53 +0100

    removed print of the ROM

--- rom.ml
+++ rom.ml
@@ -12,11 +12,11 @@ let read_file filename =
 let create filename =
   let rom_name = (Filename.chop_suffix filename ".net") ^ ".rom" in
   read_file rom_name ;
-  print_endline ("Created ROM of size "^
-		   (string_of_int (1 lsl !Rom_comm.addr_size)));
-  Array.iter (fun ar -> Array.iter
-			  (fun v ->
-			   print_string (Interaction.stringint_of_bool v)
-			  ) ar
-	     )
-	     !Rom_comm.rom
+
+  (* Array.iter (fun ar -> Array.iter *)
+  (* 			  (fun v -> *)
+  (* 			   print_string (Interaction.stringint_of_bool v) *)
+  (* 			  ) ar *)
+  (* 	     ) *)
+  (* 	     !Rom_comm.rom *)
+  ()
